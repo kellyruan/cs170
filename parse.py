cd439eeabf78358f0286f4520a8f5e95ca1c7cca
Kelly <kellyruanyu@gmail.com>
2020-12-06 18:45:37 -0800

    u

--- parse.py
+++ parse.py
@@ -9,7 +9,7 @@ import utils
 def validate_file(path):
     """File must not exceed 100KB and must contain only numbers and spaces"""
     if os.path.getsize(path) > 100000:
-        print(f"{path} exceeds 100KB, make sure you're not repeating edges!")
+        print("{path} exceeds 100KB, make sure you're not repeating edges!")
         return False
     with open(path, "r") as f:
         if not re.match(r"^[\d\.\s]+$", f.read()):
